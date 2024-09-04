728410236f6f33a4a9cff20afcb98902902f8029
Mishiez <michellegathonimain@gmail.com>
2024-09-04 18:14:01 +0300

    Create a js file and in it put functions that perform addition,subtraction, multiplication and division

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function addOperators(num1,num2){
+    return num1+num2;
+}
+function subtractOperators(num1,num2){
+    return num1-num2;
+}
+function multiplyOperators(num1,num2){
+    return num1*num2;
+}
+function divideOperators(num1,num2){
+    return num1/num2;
+}
