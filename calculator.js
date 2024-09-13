7ab93439bec264245f6a9c138741aec218198f24
Mishiez <michellegathonimain@gmail.com>
2024-09-13 18:42:53 +0300

    Create three variables for the operator and the operands. In addition to that create a function that takes in the variables and calls one of the four functions to perform an operation depending on the operand

--- calculator.js
+++ calculator.js
@@ -1,12 +1,27 @@
-function addOperators(num1,num2){
+const num1=56;
+const num2=37;
+const operator="/";
+
+function addOperators(){
     return num1+num2;
 }
-function subtractOperators(num1,num2){
+function subtractOperators(){
     return num1-num2;
 }
-function multiplyOperators(num1,num2){
+function multiplyOperators(){
     return num1*num2;
 }
-function divideOperators(num1,num2){
+function divideOperators(){
     return num1/num2;
+}
+
+function operate(){
+    const operators={
+        "+":(a,b)=>addOperators(),
+        "-":(a,b)=>subtractOperators(),
+        "*":(a,b)=>multiplyOperators(),
+        "/":(a,b)=>divideOperators()
+    };
+    let result=operators[operator](num1,num2);
+    return result;
 }
