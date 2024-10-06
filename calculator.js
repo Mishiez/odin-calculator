c0f1d30b0a37a128a710eb02bc986ba6d6e26db3
Mishiez <michellegathonimain@gmail.com>
2024-10-06 10:06:32 +0300

    Create functions that populate the display when you click the number buttons, the clear buttons and the delete buttons

--- calculator.js
+++ calculator.js
@@ -25,3 +25,28 @@ function operate(){
     let result=operators[operator](num1,num2);
     return result;
 }
+
+let display=document.querySelector("p");
+
+const buttons=document.querySelectorAll("button");
+buttons.forEach((button)=>{
+  button.addEventListener("click",click);
+});
+function click(){
+  let buttonClicked=document.getElementById(this.id).value;
+  display.innerHTML+=buttonClicked;
+}
+
+const clearButton=document.querySelector("#clear");
+clearButton.addEventListener("click",clearDisplay);
+function clearDisplay(){
+  display.innerHTML="";
+}
+
+const deleteButton=document.querySelector("#delete");
+deleteButton.addEventListener("click",deletePreviousEntry);
+function deletePreviousEntry(){
+  const str=display.innerText;
+  const newStr=str.slice(0,-1);
+  display.innerHTML=newStr;
+}
