2db1e690914acba7a7c46f5c01d4819b792d10ba
Michelle Gathoni <135155948+Mishiez@users.noreply.github.com>
2025-03-01 20:44:46 +0300

    Ensure claculator does not display more than a single pair of numbers at a time

--- javascript.js
+++ javascript.js
@@ -1,147 +1,58 @@
-let operator="";
-let num1="";
-let num2="";
+let buffer, operator;
 
-const operate = (operator,num1,num2)=>{
-    switch (operator){
-        case "+":
-            return num1 + num2;
-            break;
-        case "-":
-            return num1 - num2;
-            break;
-        case "x":
-            return num1 * num2;
-            break;  
-        case "/":
-            return num1 / num2;
-            break;
-        case "%":
-            return num1 % num2;
-            break;
-        default:
-            return "Operation not recognized";
-    }
+const compute = (operator, left, right) => {
+  switch (operator) {
+    case '+': return left + right;
+    case '-': return left - right;
+    case '*': return left * right;
+    case '/': return left / right;
+    default:  return 'ERR';
+  }
 }
 
-const buttons = document.getElementsByTagName("button");
-const display = document.getElementById("display");
-let result;
+const handleClick = (e) => {
+  const calculator = e.target.closest('.calculator');
+  const dispBuffer = calculator.querySelector('.display > .buffer');
+  const dispInput = calculator.querySelector('.display > input');
+  
+  switch (e.target.dataset.type) {
+    case 'digit':
+      dispInput.value += e.target.dataset.value;
+      break;
+    case 'operator':
+      buffer = operator ? compute(operator, +buffer, +dispInput.value) : dispInput.value;
+      operator = e.target.dataset.value;
+      dispInput.value = '';
+      dispBuffer.textContent = `${buffer} ${operator}`;
+      break;
+    case 'command':
+      switch (e.target.dataset.value) {
+        case 'AC':
+          dispInput.value = '';
+          operator = '';
+          buffer = '';
+          dispBuffer.textContent = '';
+          break;
+        case '+/-':
+          dispInput.value = +dispInput.value * -1;
+          break;
+        case '%':
+          dispInput.value = buffer ? +dispInput.value / 100 : 0;
+          break;
+        case '=':
+          dispInput.value = compute(operator, +buffer, +dispInput.value);
+          operator = '';
+          buffer = '';
+          dispBuffer.textContent = '';
+          break;
+      }
+      break;
+  }
+};
 
-
-
-
-Array.from(buttons).forEach(button=>
-    button.addEventListener("click",showNumber)
-);
-
-function showNumber(event){
-    const button = event.target;
-    if (button.value=="del"){
-        deleteControl();
-    }
-    else if(button.value=="equal-sign"){
-        display.innerHTML = operate(operator,parseFloat(num1),parseFloat(num2));
-        result = operate(operator,parseFloat(num1),parseFloat(num2));
-        operator = '';
-        num1 = result;   
-    }
-    else if(button.value=="clear"){
-        clearControl();  
-    }
-    else{
-        display.innerHTML += button.value;
-    }
-}
-
-function clearControl(){
-    num1 = "";
-    num2 = "";
-    operator = "";
-    display.innerHTML = '';
-}
-
-function deleteControl(){
-    display.innerHTML = display.innerHTML.substring(0,display.innerHTML.length - 1);
-}
-
-const numbers = document.querySelectorAll(".number")
-const operators = document.querySelectorAll(".operator")
-    
-numbers.forEach(number => {
-    number.addEventListener("click", e => {
-        if (operator === "") { // Read first number if no operator set yet
-            num1 += e.target.innerText;
-            console.log(num1)
-        } else { // Read second number
-            num2 += e.target.innerText;
-            console.log(num2)
-        }
-    });
-});
-
-operators.forEach(op => {
-    op.addEventListener("click", e => {
-        if (e.target.innerText !== "=") { // If the operator is not equals
-            operator = e.target.innerText;
-
-            console.log(num1); // Print the first number
-            console.log(operator); // Print the operator
-
-        }
-        
-
-        else { // If equals button clicked
-            console.log(num2); // Print 2nd number
-            console.log(result);
-
-            operate(operator,parseFloat(num1),parseFloat(num2));
-            num1 = result;
-            num2 = '';
-        }
-
-    });
-});
+document.querySelector('.keyboard').addEventListener('click', handleClick);
 
 
 
 
 
-
-
-
-//SOLUTION A
-/*
-const numbers = document.querySelectorAll(".number")
-const operators = document.querySelectorAll(".operator")
-
-
-numbers.forEach(number => {
-    number.addEventListener("click", e => {
-        if (operator === "") { // Read first number if no operator set yet
-            num1 += e.target.innerText;
-            console.log(num1)
-        } else { // Read second number
-            num2 += e.target.innerText;
-            console.log(num2)
-        }
-    });
-});
-
-operators.forEach(op => {
-    op.addEventListener("click", e => {
-        if (e.target.innerText !== "=") { // If the operator is not equals
-            operator = e.target.innerText;
-
-            console.log(num1); // Print the first number
-            console.log(operator); // Print the operator
-
-        } else { // If equals button clicked
-            console.log(num2); // Print 2nd number
-
-            operate(operator,parseInt(num1),parseInt(num2));
-        }
-
-    });
-});
-*/
